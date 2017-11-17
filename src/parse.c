adefd7be15bd65cf0dace5ca933e5dcd5566ddc5
Julien Coutarel <jcoutare@student.42.fr>
2017-11-17 10:16:48 +0100

    17/11/17, le mec ne sait toujours pas malloc

--- src/parse.c
+++ src/parse.c
@@ -1,79 +1,90 @@
-#include "filler.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcoutare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/17 10:04:52 by jcoutare          #+#    #+#             */
+/*   Updated: 2017/11/17 10:04:54 by jcoutare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "filler.h"
 
 int	get_piece(t_struct *d, char *buf)
 {
-  int lol;
+	int lol;
 
-  lol = ft_strlen("Piece ");
-  if (ft_strncmp(buf, "Piece ", lol) == 0)
+	lol = ft_strlen("Piece ");
+	if (ft_strncmp(buf, "Piece ", lol) == 0)
     {
-      d->piece_y_max = ft_atoi(buf + lol);
-      d->piece_x_max = ft_atoi(buf + lol +
-			      ft_strlen(ft_itoa(d->piece_y_max)));
-      if (!((d->piece = alloc_tab(d->piece, d->piece_x_max, d->piece_y_max))))
-	return (-1);
-      return (1);
+		d->piece_y_max = ft_atoi(buf + lol);
+		d->piece_x_max = ft_atoi(buf + lol +
+								 ft_strlen(ft_itoa(d->piece_y_max)));
+		if (!((d->piece = alloc_tab(d->piece, d->piece_x_max, d->piece_y_max))))
+			return (-1);
+		return (1);
     }
-  return (fill_piece(d, buf));
+	return (fill_piece(d, buf));
 }
 
 int	get_map(t_struct *d, char *buf)
 {
-  int i;
+	int i;
 
-  i = 0;
-  if (ft_strncmp(buf, "    ", ft_strlen("    ")) == 0)
+	i = 0;
+	if (ft_strncmp(buf, "    ", ft_strlen("    ")) == 0)
     {
-      if (!((d->map = alloc_tab(d->map, d->x_max, d->y_max))))
-	return (-1);
+		if (!((d->map = alloc_tab(d->map, d->x_max, d->y_max))))
+			return (-1);
     }
-  return (fill_tab(d,buf));
+	return (fill_tab(d,buf));
 }
 
 int	get_size(t_struct *d, char *buf)
 {
-  size_t lol;
+	size_t lol;
 
-  lol = ft_strlen("Plateau ");
-  if (ft_strncmp(buf,"Plateau ", lol) != 0)
-    return (0);
-  d->y_max = ft_atoi(buf + lol);
-  d->x_max = ft_atoi(buf + lol + ft_strlen(ft_itoa(d->y_max)));
-  return (1);
+	lol = ft_strlen("Plateau ");
+	if (ft_strncmp(buf,"Plateau ", lol) != 0)
+		return (0);
+	d->y_max = ft_atoi(buf + lol);
+	d->x_max = ft_atoi(buf + lol + ft_strlen(ft_itoa(d->y_max)));
+	return (1);
 }
 
 int	get_player(t_struct *d, char *buf)
 {
-  size_t lol;
+	size_t lol;
 
-  lol = ft_strlen("$$$ exec p");
-  if (ft_strncmp(buf,"$$$ exec p", lol) != 0)
-    return (0);
-  d->player = ft_atoi(buf + lol);
-  return (1);
+	lol = ft_strlen("$$$ exec p");
+	if (ft_strncmp(buf,"$$$ exec p", lol) != 0)
+		return (0);
+	d->player = ft_atoi(buf + lol);
+	return (1);
 }
 
 void	parse(t_struct *d, char *buf)
 {
-  if (d->player == 0)
+	if (d->player == 0)
     {
-      if (get_player(d, buf) == 1)
-	return ;
+		if (get_player(d, buf) == 1)
+			return ;
     }
-  else if (d->x_max == 0 || d->y_max == 0)
+	else if (d->x_max == 0 || d->y_max == 0)
     {
-      if (get_size(d, buf) == 1)
-	return ;
+		if (get_size(d, buf) == 1)
+			return ;
     }
-  else if (d->map_filled == 0)
+	else if (d->map_filled == 0)
     {
-      if (get_map(d, buf) == 1)
-	return ;
+		if (get_map(d, buf) == 1)
+			return ;
     }
-  else if (d->piece_filled == 0)
+	else if (d->piece_filled == 0)
     {
-      if (get_piece(d, buf) == 1)
-	return ;
+		if (get_piece(d, buf) == 1)
+			return ;
     }
 }
--- src/tools.c
+++ src/tools.c
@@ -1,89 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcoutare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/17 10:06:31 by jcoutare          #+#    #+#             */
+/*   Updated: 2017/11/17 10:15:21 by jcoutare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "filler.h"
 
 char	**alloc_tab(char **tab, int x, int y)
 {
-  int i;
+	int i;
 
-  i = 0;
-  if (!((tab = malloc(sizeof(char *) * y + 1))))
-    return (NULL);
-  tab[y] = 0;
-  while (i < y)
+	i = 0;
+	if (!((tab = malloc(sizeof(char *) * y + 1))))
+		return (NULL);
+	tab[y] = 0;
+	while (i < y)
     {
-      if (!((tab[i] = malloc(sizeof(char) * x + 1))))
-	return (NULL);
-      ft_memset(tab[i], '\0', x);
-      i++;
+		if (!((tab[i] = malloc(sizeof(char) * x + 1))))
+			return (NULL);
+		ft_memset(tab[i], '\0', x + 1);
+		i++;
     }
-  return (tab);
+	return (tab);
 }
+
 char	*cpy_only_with_src(char *dest, char *buf, char *src)
 {
-  int i;
-  int j;
-  int k;
+	int i;
+	int j;
+	int k;
 
-  k = 0;
-  i = 0;
-  while (buf[k])
+	k = 0;
+	i = 0;
+	while (buf[k])
     {
-      j = 0;
-      while (src[j])
-	{
-	  if (buf[k] == src[j])
-	    {
-	      dest[i] = buf[k];
-	      i++;
-	    }
-	  j++;
-	}
-      k++;
+		j = 0;
+		while (src[j])
+		{
+			if (buf[k] == src[j])
+			{
+				dest[i] = buf[k];
+				i++;
+			}
+			j++;
+		}
+		k++;
     }
-  return (dest);
+	return (dest);
 }
 
 int	fill_piece(t_struct *d, char *buf)
 {
-  int i;
+	int i;
 
-  i = 0;
-  while (d->piece[i][0] != 0)
-    i++;
-  d->piece[i] = ft_strcpy(d->piece[i], buf);
-  if (i + 1 == d->piece_y_max)
+	i = 0;
+	while (d->piece[i][0] != 0)
+		i++;
+	d->piece[i] = ft_strcpy(d->piece[i], buf);
+	if (i + 1 == d->piece_y_max)
     {
-      d->piece_filled = 1;
-      return (1);
+		d->piece_filled = 1;
+		return (1);
     }
-  return (1);
+	return (1);
 }
 
 int	fill_tab(t_struct *d, char *buf)
 {
-  int i;
+	int i;
 
-  i = 0;
-  while (d->map[i][0] != 0)
-    i++;
+	i = 0;
+	while (d->map[i][0] != 0)
+		i++;
     d->map[i] = cpy_only_with_src(d->map[i], buf, "XO.");
-  if (i + 1 == d->y_max)
+	if (i + 1 == d->y_max)
     {
-      d->map_filled = 1;
-      return (1);
+		d->map_filled = 1;
+		return (1);
     }
-  return (1);
+	return (1);
 }
 
 void	str_nb_fd(char *str, int nb)
 {
-  ft_putstr_fd(str, fd);
-  ft_putnbr_fd(nb, fd);
-  ft_putchar_fd('\n', fd);
+	ft_putstr_fd(str, fd);
+	ft_putnbr_fd(nb, fd);
+	ft_putchar_fd('\n', fd);
 }
 
 void	str_str_fd(char *str, char *str1)
 {
-  ft_putstr_fd(str, fd);
-  ft_putstr_fd(str1, fd);
-  ft_putchar_fd('\n', fd);
+	ft_putstr_fd(str, fd);
+	ft_putstr_fd(str1, fd);
+	ft_putchar_fd('\n', fd);
 }
